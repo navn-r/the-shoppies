335acf582dce23ea70583423434ed9c72ce5e2c9
Navinn Ravindaran <59669957+navn-r@users.noreply.github.com>
2020-12-31 18:05:31 -0500

    added myriad font TODO: delete unnecessary fonts after

---
+++ src/app/services/movie.mock.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import {Movie, Rating, MOCK_MOVIES} from '../models/api.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MockMovieService {
+
+  constructor() { }
+}

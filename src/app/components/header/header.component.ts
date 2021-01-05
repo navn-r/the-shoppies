9e09a3a7a50748d2fabc4478c4034733fc77af4a
Navinn Ravindaran <59669957+navn-r@users.noreply.github.com>
2021-01-04 19:06:20 -0500

    back button done

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onPressReturn(): void {
+    this.router.navigateByUrl('');
+  }
+
+  shouldBackButtonShow(): boolean {
+    return this.router.url.includes('nominate');
   }
 
 }
